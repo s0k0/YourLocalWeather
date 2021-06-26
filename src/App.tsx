b873b65e929e80f4a3b06e49842ad15cec631822
Sophie Kolbe <soko@somewhere.com>
2021-06-26 08:39:40 +0200

    fetch weather data and display in container

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import config from "./config.json";
+import "./App.css";
+
+export interface WeatherApiResponse {
+  id: number;
+  name: string;
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+    }
+  ];
+  main: {
+    temp: number;
+    feels_like: number;
+    humidty: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: Number;
+  };
+}
 
 function App() {
+  const [data, setData] = useState<WeatherApiResponse | null>(null);
+  const [location, setLocation] = useState<string>("Berlin");
+
+  useEffect(() => {
+    const url = `${config.baseUrl}/data/2.5/weather?q=${location}&appid=${config.apiKey}&units=metric`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather in: {location}</h1>
+      <div className="weather">
+        {data ? JSON.stringify(data) : "Loading data..."}
+      </div>
     </div>
   );
 }
